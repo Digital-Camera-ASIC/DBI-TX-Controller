2ece37654f8eb566838b950907642a3dfb51e489
atfox272 <trananhtai577@gmail.com>
2025-03-13 20:47:10 +0700

    mod: re-structure prj dir

--- sim/rtl.f
+++ sim/rtl.f
@@ -1,4 +1,5 @@
 ./../rtl/*.v
-./../rtl/others/encoder/*.v
-./../rtl/others/fifo/*.v
-./../rtl/others/adapter/*.v
+./../rtl/common/adapter/axi/*.v
+./../rtl/common/encoder/onehot_encoder/*.v
+./../rtl/common/fifo/sync_fifo/*.v
+./../rtl/common/skid_buffer/*.v
